c0b22bb092b7dfa8cdd35edbd09faf9b3680e5f7
Anil Sharma <anilrightnow@gmail.com>
2026-03-24 12:47:13 +0530

    latest code

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: standalone,
   async rewrites() {
     const apiBaseUrl = process.env.API_BASE_URL ?? process.env.NEXT_PUBLIC_API_BASE_URL ?? "http://localhost:5000";
     return [

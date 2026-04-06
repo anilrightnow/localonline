0b48ec24b390198d41de56c98cde3b805210d29c
Anil Sharma <anilrightnow@gmail.com>
2026-04-06 09:19:11 +0530

    Fix sitemap url domain issue

--- pages/sitemaps/[...path].ts
+++ pages/sitemaps/[...path].ts
@@ -11,7 +11,7 @@ type SitemapParams = {
 function getSiteBase(req: ReqWithHeaders): string {
   const configured =
     process.env.NEXT_PUBLIC_SITE_URL ??
-    process.env.SITE_URL ??
+    process.env.SITE_URL ??"https://localonline.in"
     process.env.PUBLIC_BASE_URL ??
     process.env.NEXT_PUBLIC_PUBLIC_BASE_URL;
   if (configured) {
